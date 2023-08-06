e226e1e2320eca6c14934f38adc6d4a74fbb88b9
Kevin-Tko <112008891+Kevin-Tko@users.noreply.github.com>
2023-08-06 04:40:47 +0000

    Added HTML and CSS

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+# import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def hello_word():
-  return 'Hello Kevin!'
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
