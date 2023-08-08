fc660c9a51be85e82634e36744d5ec215d0a35e0
Kevin-Tko <112008891+Kevin-Tko@users.noreply.github.com>
2023-08-08 03:33:34 +0000

    ssl configs

--- database.py
+++ database.py
@@ -8,9 +8,21 @@ from sqlalchemy import create_engine,text
 # dbname = os.environ["DATABASE"]
 # print(host)
 
-connection_str = 'mysql+pymysql://88cwrdv2659ospeog4hh:pscale_pw_l1iaWIuU4rJLQVyc3lnDayAzarBfcE6M4oHMeI0yC96@aws.connect.psdb.cloud/careerswebsitev2?charset=utf8mb4'
+connection_str = 'mysql+pymysql://m15eqscipp4jyb9teg98:pscale_pw_Nosb0J541Ykj3dfDZEwjq5PRd8szRPq7Y69cM1y38QZ@aws.connect.psdb.cloud/careerswebsitev2?charset=utf8mb4'
 
-engine = create_engine(connection_str, echo=True)
+# ssl_args = {
+#   'ssl_ca':"/etc/ssl/cert.pem"
+# }
+engine = create_engine(
+    connection_str,
+    connect_args={
+        "ssl": {
+            "ca": "/etc/ssl/certs/ca-certificates.crt"
+        }
+    }
+)
+
+# engine = create_engine(connection_str, connect_args=ssl_args)
 
 JOBS=[]
 def db_job():
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from database import db_job
 from flask import Flask, render_template, jsonify
 
-app = Flask(__name__, static_folder='static')
-
+app = Flask(__name__, static_folder='static', template_folder='templates')
+jobs_ls = db_job()
+print(jobs_ls)
 @app.route("/")
 def hello_word():
-  jobs_ls = db_job
+  jobs_ls = db_job()
   return render_template('home.html', jobs=jobs_ls)
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs_ls = db_job
+  jobs_ls = db_job()
   return jsonify(jobs_ls)
 
 if __name__ == '__main__':
