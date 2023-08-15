10724cce5347871fe8c2aac280b8d83ac067a7c5
Kevin-Tko <112008891+Kevin-Tko@users.noreply.github.com>
2023-08-15 12:33:09 +0000

    JOBS

--- database.py
+++ database.py
@@ -13,19 +13,22 @@ engine = create_engine(
     }
 )
 
-JOBS=[]
 def db_job():
   with engine.connect() as conn:
     result = conn.execute(text('select * from jobs'))
     all_jobs = result.all()
+    
     for items in all_jobs:
-      JOBS.append({
-        'id': items[0],
-        'Title': items[1], 
-        'Company': items[2],
-        'Location': items[3],
-        'Responsibilities': items[4],
-        'Requirements':items[5]}
-      )
+      JOBS={'id': items[0],'Title': items[1],'Company': items[2],'Location': items[3],'Responsibilities': items[4],'Requirements':items[5]}
     return JOBS
+
+      # JOBS.append({
+      #   'id': items[0],
+      #   'Title': items[1], 
+      #   'Company': items[2],
+      #   'Location': items[3],
+      #   'Responsibilities': items[4],
+      #   'Requirements':items[5]}
+      # )
+    # return JOBS
 
